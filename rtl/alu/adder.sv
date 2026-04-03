119791d96b8ecd97c737fce9419988b7bcf4fe2f
Stefan Kopieczek <stefankopieczek@gmail.com>
2026-04-03 21:15:18 +0100

    Add comment for two's complement addition

--- rtl/alu/adder.sv
+++ rtl/alu/adder.sv
@@ -6,6 +6,8 @@ module adder (
 );
 
   always_comb begin
+    // Note that this is valid irrespective of the sign of the operands.
+    // Hooray for two's complement arithmetic!
     out   = a + b;
     carry = a[31] & b[31];
   end
